ab9cc39ebebea262ae3e3a4d95f781e23239f37a
Gopule <altusszawlowski@gmail.com>
2021-10-23 17:59:57 -0400

    bitcoin component modularized

--- src/client/Main.js
+++ src/client/Main.js
@@ -27,47 +27,13 @@ const Main = () => {
         <div>
           <Exchanges />
 
-          <div className="compare-coins">
-            <div
-              className={checkStyle(coinbase.BTC - binance.BTC)}
-              onClick={() =>
-                window.open("https://pro.coinbase.com/trade/BTC-USD", "_blank")
-              }
-              title="Coinbase BTC"
-            >
-              <img src={BTC_Logo} />
-              <section className="price-coin">
-                <h3>BTC</h3>
-                <p>${coinbase.BTC}</p>
-              </section>
-            </div>
-
-            <span className="greater-lesser-equal">
-              {checkPrices(coinbase.BTC, binance.BTC)}
-            </span>
-
-            <div
-              className={checkStyle(binance.BTC - coinbase.BTC)}
-              onClick={() =>
-                window.open("https://www.binance.com/en/buy-Bitcoin", "_blank")
-              }
-              title="Binance BTC"
-            >
-              <img src={BTC_Logo} />
-              <section className="price-coin">
-                <h3 className="coin-name">BTC</h3>
-                <p>${binance.BTC}</p>
-              </section>
-            </div>
-          </div>
-
-          <div className="sentence">
-            {generateSentence(
-              "Coinbase",
-              "Bitcoin",
-              coinbase.BTC - binance.BTC
-            )}
-          </div>
+          <Bitcoin
+            checkStyle={checkStyle}
+            checkPrices={checkPrices}
+            generateSentence={generateSentence}
+            coinbase={coinbase}
+            binance={binance}
+          />
         </div>
       )}
     </div>
