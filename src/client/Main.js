bcb8a4f7c4a8f5adee0ea42680643a83722a1dd5
Gopule <altusszawlowski@gmail.com>
2021-10-23 16:54:50 -0400

    data comes through in the frontend

--- src/client/Main.js
+++ src/client/Main.js
@@ -1,7 +1,24 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Main = () => {
-  const [] = useState({});
+  const [coinbase, setCoinbase] = useState({});
+  const [binance, setBinance] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data: coins } = await axios.get("/api/coins/");
+      setCoinbase({
+        BTC: Number(coins.coinbase_BTC_Data.price).toFixed(2),
+        ETH: Number(coins.coinbase_ETH_Data.price).toFixed(2),
+      });
+      setBinance({
+        BTC: Number(coins.binance_BTC_Data.price).toFixed(2),
+        ETH: Number(coins.binance_ETH_Data.price).toFixed(2),
+      });
+    };
+    getData();
+  }, []);
 
   return <div></div>;
 };
