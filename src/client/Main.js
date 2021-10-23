5b779424a426aee63af58add7d8b8419bd99993e
Gopule <altusszawlowski@gmail.com>
2021-10-23 18:05:43 -0400

    ethereum component

--- src/client/Main.js
+++ src/client/Main.js
@@ -34,6 +34,47 @@ const Main = () => {
             coinbase={coinbase}
             binance={binance}
           />
+          <div className="compare-coins">
+            <div
+              className={checkStyle(coinbase.ETH - binance.ETH)}
+              onClick={() =>
+                window.open("https://pro.coinbase.com/trade/ETH-USD", "_blank")
+              }
+              title="Coinbase ETH"
+            >
+              <img src={ETH_Logo} />
+              <section className="price-coin">
+                <h3 className="coin-name">ETH</h3>
+                <p>${coinbase.ETH}</p>
+              </section>
+            </div>
+
+            <span className="greater-lesser-equal">
+              {checkPrices(coinbase.ETH, binance.ETH)}
+            </span>
+
+            <div
+              className={checkStyle(binance.ETH - coinbase.ETH)}
+              onClick={() =>
+                window.open("https://www.binance.com/en/buy-Ethereum", "_blank")
+              }
+              title="Binance ETH"
+            >
+              <img src={ETH_Logo} />
+              <section className="price-coin">
+                <h3 className="coin-name">ETH</h3>
+                <p>${binance.ETH}</p>
+              </section>
+            </div>
+          </div>
+
+          <div className="sentence">
+            {generateSentence(
+              "Coinbase",
+              "Ethereum",
+              coinbase.ETH - binance.ETH
+            )}
+          </div>
         </div>
       )}
     </div>
